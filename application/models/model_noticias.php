d7acc6871f24c36e7de64cc39fa8a4f988f2e2bf
Jhonatan Müller <ujaron2@gmail.com>
2015-07-26 11:29:33 -0300

    Improvement on model_noticias

--- application/models/model_noticias.php
+++ application/models/model_noticias.php
@@ -9,27 +9,33 @@ class Model_noticias extends CI_Model
 	}
 	
 	function countNoticias($busca)
-	{
-		if($busca == 'sem_busca')
-			return $this->db->where('removed_on IS NULL')
-							->count_all_results('unmd_noticias');
-		else
-			return $this->db->where('removed_on IS NULL')
-							->like('titulo', $busca)
-							->count_all_results('unmd_noticias');
+	{		
+		$query = $this->db->join('unmd_categorias t2', 't1.id_categoria = t2.id_categoria', 'inner')
+						->where('t1.removed_on IS NULL')
+						->where('t2.removed_on IS NULL');
+
+		if($busca != 'sem_busca')
+			$query->like('titulo', $busca);
+							
+		$result = $query->count_all_results('unmd_noticias t1');
+
+		return $result;
 	}
 	function getNoticias($busca, $inicio, $offset)
 	{
-		$query = $this->db->select(array('id_noticia', 'titulo', 'texto', 'publicada_em'))
-				 		  ->where('removed_on IS NULL');						 
+		$query = $this->db->select(array('t1.id_noticia', 't1.titulo', 't1.texto', 't1.publicada_em'))
+						  ->from('unmd_noticias t1')
+						  ->join('unmd_categorias t2', 't1.id_categoria = t2.id_categoria', 'inner')
+				 		  ->where('t1.removed_on IS NULL')
+				 		  ->where('t2.removed_on IS NULL');						 
 			
 		if($busca != 'sem_busca')
-			$query->like('titulo', $busca);
+			$query->like('t1.titulo', $busca);
 
 		$result = $query->limit($offset, $inicio)
-			  ->order_by('publicada_em', 'DESC')
-			  ->get('unmd_noticias')
-			  ->result();
+			  			->order_by('t1.publicada_em', 'DESC')
+			  			->get()
+			  			->result();
 
 		return $result;
 	}
