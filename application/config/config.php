ca4172992fe0adb87cc92158e5bd7ef9851e21d9
Jhonatan Müller <ujaron2@gmail.com>
2015-07-27 00:35:56 -0300

    Adjust base url name

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= '/unimed_noticias/';
+$config['base_url']	= '/unimedvr_noticias/';
 
 /*
 |--------------------------------------------------------------------------
